59006ae3f0d5695a13ce9f67d7aef91cbebf5d62
AnwarMestycer <anwarmestycer0@.com>
2024-04-11 20:06:57 +0100

    design is ready

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+
+export const routes: Routes = [
+    {
+        path:'product',
+        component:ProductCardComponent
+    }
+];
